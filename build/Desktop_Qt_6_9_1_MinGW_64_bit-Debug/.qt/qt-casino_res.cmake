204a778bb55784e0ebd53a022ea9476304dc2a0d
Eli Kloft <elikloft17@gmail.com>
2025-07-29 14:25:39 -0700

    remove build directory files from git

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/qt-casino_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "qt-casino")
-set(working_dir "C:/Users/elikl/Documents/GitHub/qt-casino")
-set(src_and_dest_list
-
-)
-set(timestamp_file "C:/Users/elikl/Documents/GitHub/qt-casino/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/qt-casino_res.txt")
